8beb56a47b504479a5639355b97f8113ebd0b2d7
Michael McKenna <michael@solve.solutions>
2023-06-30 02:44:57 +1200

    Support for configuring by command line and YAML

---
+++ RssToMd/Infrastructure/YamlOptions.cs
@@ -0,0 +1,18 @@
+﻿using CommandLine.Text;
+using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RssToMd.Infrastructure
+{
+    class YamlOptions
+    {
+        public string? TimeZone { get; set; }
+
+        public string[] Feeds { get; set; }
+
+    }
+}
---
+++ RssToMd/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CommandLine;
+using RssToMd.Infrastructure;
+
+var configuration = Configuration.Load(Environment.GetCommandLineArgs());
+
+Console.WriteLine($"Generating digest for {configuration.Date} ({configuration.TimeZone})");
